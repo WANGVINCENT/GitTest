73468bc485a2d581d141c4d51b735eb88c098819
WANGVINCENT <wangnan.vincent@gmail.com>
2015-04-15 02:16:12 -0700

    fdgdg

--- nan/nan.java
+++ nan/nan.java
@@ -1,37 +1,5 @@
-asdasda
-
-
-
-sadasdsadas
-a
-sd
-as 
-sadasdaslkdjalsdjskajdlajdlkask
-asd
-
-sadfsdflksdjf djfksjlksd
-
-
-
-
 
 ect HTTPS URL
 This is not my project's URL, but YOUR project's URL
-In Eclipse, make sure that you have the "Git Repositories" view available
-Go to Window --> Show View --> Other
-Click on Git --> Git Repositories
-Now in the Git window, click on Clone a Git repository
-Your copied Git repository will auto-fill
-Type your username and password
-Choose "Store in Secure Store"
-Click Next, and then Next again
-At this window, you will see Finish, but before you click, use correct destination directory
-Choose your "workspace" directory, such as SJSU_Dev/projects
-Choose the same project folder, "lpc1758_teamx"
-Click Finish, and wait a few minutes for Eclipse to bring-in the Gitlab project
-Your project will show under "Git Repositories" section.
-Tutorial gitlab eclipse clone.PNG
-
-Import Gitlab project to Eclipse
-
-If you cloned my LPC project, the final step is to IMPORT the proj
+sadasdasdasdasdsadasdasdshgjhasjhsahjshjas
+dhsghsgsj
